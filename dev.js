7755828709ca7729f782b958de650305c5257a58
David McClure <david.mcclure@virginia.edu>
2013-01-31 18:13:03 -0500

    Basic development .js harness.

--- dev.js
+++ dev.js
@@ -1,3 +1,22 @@
 $(function() {
-  // TODO|dev
+
+  var map = new OpenLayers.Map('map');
+
+  var poly = new OpenLayers.Layer.Vector();
+  var base = new OpenLayers.Layer.WMS(
+    'OpenLayers WMS',
+    'http://vmap0.tiles.osgeo.org/wms/vmap0?', {
+      layers: 'basic'
+    }
+  );
+
+  map.addLayers([base, poly]);
+
+  var geo = new OpenLayers.Control.DrawFeature(
+    poly, OpenLayers.Handler.Geometry
+  );
+
+  map.addControl(geo);
+  map.setCenter([0, 0], 3);
+
 });
